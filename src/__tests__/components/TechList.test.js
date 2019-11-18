21552b80c71df2ab58c07e27241f72cb24b73e83
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-17 21:25:16 -0300

    mock do localstorage

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
+import 'jest-localstorage-mock';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
 describe('TechList component', () => {
+	beforeEach(() => {
+		localStorage.clear();
+	});
+
 	it('shoud be able to add new tech', () => {
 		const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
@@ -18,5 +23,25 @@ describe('TechList component', () => {
 
 		expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
 		expect(getByLabelText('Tech')).toHaveValue('');
+	});
+
+	it('should store techs in storage', () => {
+		let { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+		fireEvent.change(getByLabelText('Tech'), {
+			target: { value: 'Node.js' },
+		});
+
+		fireEvent.submit(getByTestId('tech-form'));
+
+		cleanup();
+
+		({ getByText, getByTestId, getByLabelText } = render(<TechList />));
+
+		expect(localStorage.setItem).toHaveBeenCalledWith(
+			'techs',
+			JSON.stringify(['Node.js'])
+		);
+		expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
 	});
 });
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TechList = () => {
 	const [techs, setTechs] = useState([]);
 	const [newTech, setNewTech] = useState([]);
+
+	useEffect(() => {
+		const storage = localStorage.getItem('techs');
+
+		if (storage) {
+			setTechs(JSON.parse(storage));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('techs', JSON.stringify(techs));
+	}, [techs]);
 
 	const handleAddTech = () => {
 		setTechs([...techs, newTech]);
