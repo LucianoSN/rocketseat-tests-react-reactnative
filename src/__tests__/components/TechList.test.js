89a603e2eb500086e7c8c37f2bb5de56b12b614e
Luciano de Souza Nery <lsnery@gmail.com>
2019-11-17 21:01:52 -0300

    testando formulario

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -6,12 +6,17 @@ import TechList from '~/components/TechList';
 
 describe('TechList component', () => {
 	it('shoud be able to add new tech', () => {
-		const { getByText, getByTestId, debug } = render(<TechList />);
+		const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
-		debug();
-		fireEvent.click(getByText('Adicionar'));
-		debug();
+		fireEvent.change(getByLabelText('Tech'), {
+			target: { value: 'Node.js' },
+		});
+
+		fireEvent.submit(getByTestId('tech-form'));
+
+		// fireEvent.click(getByText('Adicionar'));
 
 		expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+		expect(getByLabelText('Tech')).toHaveValue('');
 	});
 });
